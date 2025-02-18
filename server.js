ff557a40626a5d49bc5d77ea820f6a8bef78c6a6
balasaheb-karjule <127203753+balasaheb-karjule@users.noreply.github.com>
2025-02-19 00:05:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, World! from Node.js inside Minikube');
+    res.send('Hello, World! from Node.js inside Minikube *******************');
     console.log(`Server is running on port ${PORT}`);
 });
 
